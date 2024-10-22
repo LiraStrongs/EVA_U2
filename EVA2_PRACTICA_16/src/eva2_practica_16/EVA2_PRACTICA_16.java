6058c6de2366db005fee534d4e57a8d380e30381
LiraStrongs <sebastianlira06@gmail.com>
2024-10-22 16:44:36 -0600

    Add files via upload

---
+++ EVA2_PRACTICA_16/src/eva2_practica_16/EVA2_PRACTICA_16.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica_16;
+
+/**
+ *
+ * @author sebastianlira
+ */
+public class EVA2_PRACTICA_16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+      
+        
+        int[] datos,x,y,z; //todos son arreglos
+        int val1, val2, arreglo[], val3;// solo arreglo es arreglo
+         arreglo = new int[10];
+         
+        for (int i = 0; i < 10; i++){
+             arreglo[i] = (int)(Math.random() * 100);
+        }
+        
+        for (int i = 0; i < 10; i++){
+            System.out.println(arreglo[i]);
+        }
+        
+        
+        
+        
+        
+    }
+    
+}
