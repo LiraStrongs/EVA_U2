01e2f96ba826287078ca180275de2ee9fa990a9c
LiraStrongs <sebastianlira06@gmail.com>
2024-10-22 17:48:04 -0600

    Add files via upload

---
+++ EVA2_PRACTICA_18/src/eva2_practica_18/EVA2_PRACTICA_18.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica_18;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sebastianlira
+ */
+public class EVA2_PRACTICA_18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Scanner captu = new Scanner(System.in);
+       int cantidad, producto; 
+       String menu[] = {"dogos", "hamburguesas", "bonless", "tacos", "montados", "camarones"};
+       
+    
+    
+      int precios[] = {50,80,120,150,70,200};
+      
+      for (int i = 0; i < menu.length; i++) {
+      System.out.println(i+"-"+ menu[i] + "$" + precios[i]);
+        
+      
+}
+     for (int i = 0; i < menu.length; i++) {
+      System.out.println(" Nombre del producto"); 
+     
+    
+    }
+      
+    
+    
+    
+}
